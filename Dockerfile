3314acd0e530dca1f1088b58a2dea6e3ed23217f
rajendraprasad10 <rajendra.taidala10@gmail.com>
2022-01-09 17:54:27 +0530

    dockerized the application with compose

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.8
+
+ENV PYTHONUNBUFFERED=1
+WORKDIR /app
+COPY requirments.txt /app/
+RUN pip install -r requirments.txt
+COPY . /app/
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nbshsee5@m)=0iu*r2q^lbww+pupvc$*-g3oa+tbs-g7w#6c#2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'greatkart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD' : 'postgres',
+        'HOST' : 'pgdb',
+        'PORT' : 5432
     }
 }
 
